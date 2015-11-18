c17ac939d6195af7123ed8b8dcabbe128a0742bf
Marco Lau <marco.lau@industrieit.com>
2015-11-18 11:34:00 +0800

    Ignore pods

--- Pods/Target Support Files/DKScrollingTabController/DKScrollingTabController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DKScrollingTabController : NSObject
-@end
-@implementation PodsDummy_DKScrollingTabController
-@end
--- Pods/Target Support Files/Pods-PrudentialPrototype/Pods-PrudentialPrototype-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PrudentialPrototype : NSObject
-@end
-@implementation PodsDummy_Pods_PrudentialPrototype
-@end
